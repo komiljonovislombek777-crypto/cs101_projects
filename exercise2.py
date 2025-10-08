99d12ced625f0fca729d6f414ff4cd33bfa62892
islombekkomiljonov7 <komiljonovislombek777@gmail.com>
2025-10-08 08:59:48 +0500

    Initial commit:create celsius

---
+++ exercise2.py
@@ -0,0 +1,10 @@
+radius=float(input("enter radius:"))
+pi = 3.14159
+area =(radius**2)*pi
+Circumference = 2*(radius)*pi
+
+print(f"the area of circle:{area}")
+print(f"the circumference of circle:{Circumference}")
+
+
+
