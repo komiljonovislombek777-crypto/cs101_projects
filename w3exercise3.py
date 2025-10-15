a845e9b19be044272537abcc5aedef6062143750
islombekkomiljonov7 <komiljonovislombek777@gmail.com>
2025-10-15 09:22:27 +0500

    Add w3exercise3.py, Simple Triangle Pattern

---
+++ w3exercise3.py
@@ -0,0 +1,12 @@
+print("\n--- Triangle Pattern Printer ---")
+height = 5
+width = 1
+
+for row_num in range(height):
+
+    for col_num in range(width+row_num):
+
+        print("* ", end="")
+        
+   
+    print()
